a0d3b18ba2765e062d6d0f40f470702fb40aaa03
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-09-26 10:22:45 -0500

    Update material

--- src/config/global.js
+++ src/config/global.js
@@ -154,6 +154,11 @@ export default {
       },
       {
         icono: 'fas fa-download',
+        titulo: 'Descargar pdf',
+        download: 'downloads/CFA_05_722200.zip',
+      },
+      {
+        icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
       },
